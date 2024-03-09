a00717ddc796d88b2777220ae185c8a07bbe3fda
Manuel Tapia Puc <146888264+Lcukytp23@users.noreply.github.com>
2024-03-08 21:10:14 -0600

    Add files via upload

---
+++ hanoi.py
@@ -0,0 +1,36 @@
+class Pila:
+    def __init__(self):
+        self.items = []
+
+    def esta_vacia(self):
+        return self.items == []
+
+    def apilar(self, item):
+        self.items.append(item)
+
+    def desapilar(self):
+        return self.items.pop()
+
+
+def hanoi(n, origen, auxiliar, destino):
+    if n == 1:
+        destino.apilar(origen.desapilar())
+        print("Mueve un disco de", origen, "a", destino)
+    else:
+        hanoi(n-1, origen, destino, auxiliar)
+        destino.apilar(origen.desapilar())
+        print("Mueve un disco de", origen, "a", destino)
+        hanoi(n-1, auxiliar, origen, destino)
+
+
+if __name__ == "__main__":
+    pila_origen = Pila()
+    pila_auxiliar = Pila()
+    pila_destino = Pila()
+
+    discos = 3
+    for i in range(discos, 0, -1):
+        pila_origen.apilar(i)
+
+    print("Pasos a seguir para resolver el juego de las Torres de Hanoi:")
+    hanoi(discos, pila_origen, pila_auxiliar, pila_destino)
